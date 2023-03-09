7b48e36f8c312c1f9911932caf278afad57c7b7d
Harshit Satya <hsatya@systango.com>
2023-03-09 18:35:48 +0530

    updating our directory page with directory component and category item component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import categories from "./categories.json";
+import Directory from "./components/directory/directory.component";
+
+const App = () => {
+  return (
+    <div>
+      <Directory categories={categories} />
+    </div>
+  );
+};
+
+export default App;
